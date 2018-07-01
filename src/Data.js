4d49a7babecd5c5d8b4db8fbc8bb28ddb8c90355
Herbert Ilhan Tanujaya <mail@herbert.id>
2018-07-01 16:47:00 -0500

    Add dummy data

--- src/Data.js
+++ src/Data.js
@@ -4,6 +4,14 @@ export const dans = ['七級', '六級', '五級', '四級', '三級', '二級',
 
 export const difficulties = ['Normal', 'Hyper', 'Another'];
 
+export const versions = ['1st style', 'substream', '2nd style', '3rd style',
+                         '4th style', '5th style', '6th style', '7th style',
+                         '8th style', '9th style', '10th style', 'IIDX RED',
+                         'HAPPY SKY', 'DistorteD', 'GOLD', 'DJ TROOPERS',
+                         'EMPRESS', 'SIRIUS', 'Resort Anthem', 'Lincle',
+                         'tricoro', 'SPADA', 'PENDUAL', 'copula', 'SINOBUZ',
+                         'CANNON BALLERS']
+
 export const contests = [
   {
     name: 'THE',
@@ -98,6 +106,16 @@ export const results = [
     [
     ],
     [
+      // {
+      //   name: 'SHIYOU',
+      //   dan: 18,
+      //   results: [2, 3, 4, 5]
+      // },
+      // {
+      //   name: 'DONJAR',
+      //   dan: 12,
+      //   results: [1, 2, 3, 4]
+      // },
     ]
   ],
 ];
